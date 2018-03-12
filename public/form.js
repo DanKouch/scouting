7cd4dda583d4b2cb5691338be00980ebefaec8d2
DanKouch <DanKouch@users.noreply.github.com>
2018-03-12 09:04:32 -0700

    Added custom fields greying out when 'custom' is not selected.

--- public/form.js
+++ public/form.js
@@ -1,1 +1,15 @@
 $("#successAlert").fadeTo(2000, 500).slideUp(500, function(){});
+
+$("#driveTrainInput").change(() => {
+	$("#customDriveTrainInput").prop("disabled", !($("#driveTrainInput").val() == "Other"));
+	if($("#driveTrainInput").val() != "Other"){
+		$("#customDriveTrainInput").val("");
+	}
+});
+
+$("#climberTypeInput").change(() => {
+	$("#customClimberTypeInput").prop("disabled", !($("#climberTypeInput").val() == "Other"));
+	if($("#climberTypeInput").val() != "Other"){
+		$("#customClimberTypeInput").val("");
+	}
+});
