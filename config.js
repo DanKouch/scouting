9de695d39cde3715f923e30108dc71571d0f3485
DanKouch <DanKouch@users.noreply.github.com>
2018-03-12 01:54:28 -0700

    Added form greying-out and fixed env bug

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     appPort: process.env.PORT || 3001,
     apiPort: process.env.API_PORT || 3005,
-    teamName: process.env.TEAM_NAME || "",
+    teamName: process.env.TEAM_NAME || "Default Robotics Team",
     databaseURI: process.env.MONGODB_URI || process.env.MONGOLAB_URI || "mongodb://localhost/scouting",
 
     // Tournament
     tournament: process.env.TOURNAMENT || "Unknown",
-    teams: process.env.TEAMS.split(", "),
+    teams: process.env.TEAMS.split(", ") || ["Default Team"],
 
     // Do not edit anything beyond this point unless you know what you are doing
     secret: {
