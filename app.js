c1333601b45bf375fb36ee69b51eb9d98b69397c
Daniel Kouchekinia <daniel@pyrus.us>
2019-03-07 15:14:12 -0800

    Set up stuff

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-'use strict';
-
 // Import NPM Modules
-const express = require('express');
-const passport = require("passport");
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const flash = require("connect-flash");
-const cookieParser = require('cookie-parser');
-const passportLocal = require('passport-local');
-const winston = require('winston');
+const express = require("express")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const winston = require("winston")
+const config = require("config")
 
-const app = express();
-const api = express();
+// Create Express Server Instances
+const app = express()
+const api = express()
 
 // Dot Env Configuration
 require('dotenv').config()
@@ -21,50 +15,18 @@ require('dotenv').config()
 // Configure Winston logging
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {'timestamp': true, 'colorize': true, 'level': "silly"})
-
-
-// Used for reference
-const config = require("./config.js");
+winston.add(winston.transports.File, { filename: './logs/main.log' });
 
 // Configure body parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cookieParser(config.secret.passportSessionSecret));
+app.use(cookieParser(process.env.COOKIE_SECRET || "insecure"));
 
-// Import local modules
-const databaseScript = require("./database.js");
-const authenticationScript = require("./authentication.js");
+// Set Up Database Connecton
 
-// Sessions
-app.use(session({
-  secret: config.secret.passportSessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-  	mongooseConnection: mongoose.connection
-  })
-}));
+// Configure Sessions
 
-// Initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-const User = mongoose.model("User");
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new passportLocal.Strategy({session: true}, authenticationScript.authenticate));
-
-// Initialize flash
-app.use(flash());
+// Configure Passport
 
 // Set up public folders
 app.use("/", express.static('./bower_components'));
@@ -75,19 +37,7 @@ app.set("view engine", "pug");
 app.set('views', './app_server/views');
 
 // Base routing
-app.use("/", require("./app_server/router.js"));
-api.use("/", require("./app_api/router.js"));
 
 // Start App HTTP server
-app.set('port', config.appPort);
-
-app.listen(app.get('port'), () => {
-  winston.info("App HTTP server started on port " + app.get('port'));
-});
 
 // Start API HTTP server
-api.set('port', config.apiPort);
-
-api.listen(api.get('port'), () => {
-  winston.info("API HTTP server started on port " + api.get('port'));
-});
--- readme.md
+++ readme.md
@@ -1,18 +1,3 @@
 # Scouting for FRC Team 1458 Red Tie Robotics
 
 A web server used to communicate with a Mongo database to collect information for and during FRC competitions.
-
-## Enviroment Configuration Variables
-	- NODE_ENV - "production" or "debug"
-	- MONGODB_URI
-	- MONGOLAB_URI - Secondary URI
-	- PORT - App http port
-	- API_PORT - Api http port
-	- PASSPORT_SESSION_SECRET
-	- ROOT_PASSWORD
-	- WEB_DATA_PATH
-	- TEAM_NAME
-	- TOURNAMENT - Name of current tournament
-	- TEAMS - List of teams attending current tournament separated by ', '
-
-*Note: This project does support `.env` file integration.*
